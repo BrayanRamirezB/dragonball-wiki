96b857541647912ca1d7df8732e3ad4d3c47b3fd
RamirezBB <brbrayan1945@gmail.com>
2025-02-15 21:19:01 -0600

    add transformations

--- src/components/FighterCard.tsx
+++ src/components/FighterCard.tsx
@@ -1,10 +1,11 @@
 import React, { useMemo } from 'react'
-import { type Fighter } from '@/const/Fighter'
+import { type Fighter, type Transformation } from '@/const/Fighter'
 import { getPreviousFighter, getNextFighter } from '@/const/SpecialClases'
 import Card from '@/components/Card'
 import CardContent from '@/components/CardContent'
 import FighterLink from './FighterLink'
 import Radar from './Radar'
+import ButtonGroup from '@/components/ButtonGroup'
 
 interface FighterCardProps {
   fighter: Fighter
@@ -16,6 +17,25 @@ const FighterCard: React.FC<FighterCardProps> = ({ fighter }) => {
     [fighter.id]
   )
   const nextFighter = useMemo(() => getNextFighter(fighter.id), [fighter.id])
+
+  // Función para manejar la transformación
+  const handleTransformation = (transformation: Transformation) => {
+    const cardFighterImage = document.getElementById(
+      'card-fighter-image'
+    ) as HTMLImageElement | null
+    const cardNameElement = document.getElementById(
+      'card-fighter-name'
+    ) as HTMLElement | null
+    const cardKiElement = document.getElementById(
+      'card-fighter-ki'
+    ) as HTMLElement | null
+
+    if (cardFighterImage && cardNameElement && cardKiElement) {
+      cardFighterImage.src = transformation.image
+      cardNameElement.innerText = transformation.name
+      cardKiElement.innerText = transformation.ki
+    }
+  }
 
   const gradientBackground: { [key: string]: string } = {
     Human: 'bg-gradient-to-r from-blue-300/40 to-blue-600/40',
@@ -33,6 +53,26 @@ const FighterCard: React.FC<FighterCardProps> = ({ fighter }) => {
     Unknow: 'bg-gradient-to-r from-gray-500/40 to-gray-800/40'
   }
 
+  const baseTransformation = {
+    id: fighter.id,
+    name: fighter.name,
+    image: fighter.image,
+    ki: fighter.ki,
+    deletedAt: null
+  }
+
+  const buttons = [
+    {
+      label: fighter.name,
+      onClick: () => handleTransformation(baseTransformation)
+    }
+  ].concat(
+    fighter.transformations.map((transformation) => ({
+      label: transformation.name,
+      onClick: () => handleTransformation(transformation) // Pasar una función anónima
+    }))
+  )
+
   return (
     <section className='flex flex-col justify-center items-center gap-4'>
       <div className='flex flex-row justify-center items-center'>
@@ -47,7 +87,10 @@ const FighterCard: React.FC<FighterCardProps> = ({ fighter }) => {
             <CardContent textSize='sm'>
               <div className='pb-2 flex flex-row items-center justify-start gap-x-12 w-full'>
                 <Radar />
-                <h2 className='text-2xl font-bold text-center text-neutral-100'>
+                <h2
+                  id='card-fighter-name'
+                  className='text-2xl font-bold text-center text-neutral-100'
+                >
                   {fighter.name}
                 </h2>
               </div>
@@ -57,25 +100,29 @@ const FighterCard: React.FC<FighterCardProps> = ({ fighter }) => {
                 } backdrop-blur-lg p-2 rounded-lg`}
               >
                 <img
+                  id='card-fighter-image'
                   style={
                     { viewTransitionName: 'fighter' } as React.CSSProperties
                   }
                   src={fighter.image}
                   alt={fighter.name}
-                  className='h-80 object-cover'
+                  className='h-80 object-cover transition-all duration-300 ease-in-out'
                 />
               </div>
               <div className='flex flex-row items-center justify-between gap-x-2 mt-4 text-neutral-100/80 bg-zinc-700/40 backdrop-blur-md rounded-md rounded-b-4xl  py-2 px-3 font-semibold min-h-16'>
                 <div className='flex flex-col gap-y-1 justify-center'>
                   <h3 className='uppercase'>Ki:</h3>
-                  <span className='text-xl text-pretty text-center'>
+                  <span
+                    id='card-fighter-ki'
+                    className='text-xl text-pretty text-center'
+                  >
                     {fighter.ki}
                   </span>
                 </div>
                 <div className='flex flex-col gap-y-1 justify-center items-start'>
-                  <span className=''>Afiliación: {fighter.affiliation}</span>
-                  <span className=''>Raza: {fighter.race}</span>
-                  <span className=''>Género: {fighter.gender}</span>
+                  <span>Afiliación: {fighter.affiliation}</span>
+                  <span>Raza: {fighter.race}</span>
+                  <span>Género: {fighter.gender}</span>
                 </div>
               </div>
             </CardContent>
@@ -89,7 +136,18 @@ const FighterCard: React.FC<FighterCardProps> = ({ fighter }) => {
         />
       </div>
 
-      <div className='text-neutral-100/80 text-pretty text-center max-w-md lg:max-w-2xl'>
+      <div>
+        {fighter.transformations.length > 0 && (
+          <ButtonGroup
+            buttons={buttons}
+            color='default'
+            size='md'
+            rounded='md'
+          />
+        )}
+      </div>
+
+      <div className='text-neutral-100/80 text-pretty max-w-md sm:max-w-xl lg:max-w-3xl'>
         {fighter.description}
       </div>
     </section>
